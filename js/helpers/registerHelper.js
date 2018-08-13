064e71bf90627db0f9706d41201db75999c1f8b5
Neword <newordn@gmail.com>
2018-08-13 12:53:11 +0100

    Added show and hide password functionnality when clicked on the eye icon

---
+++ js/helpers/registerHelper.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded",()=>{
+	 	let passwordIcon= document.getElementById("showOrHidePassword");	
+	 	let password= document.getElementById("password");	
+	 	/* to show or hide the password */
+		let showOrHidePassword=()=>{
+
+  		if(passwordIcon.innerHTML=="visibility_off")
+  	 	{
+  	 		passwordIcon.innerHTML="visibility";
+  	 		password.removeAttribute("type");
+  	 		password.setAttribute("type","text");
+
+  	 	}
+  	 	else
+  	 	{
+  	 		passwordIcon.innerHTML="visibility_off";
+  	 		password.removeAttribute("type");
+  	 		password.setAttribute("type","password");
+  	 	} 
+  	 		
+	}
+
+		/** the listener on passwordIcon **/
+		passwordIcon.addEventListener("click",showOrHidePassword);
+		
+}
+
+);
